d633d0481c5750bcf7e00fa3b94d91757b134810
catexstacy <stacycatex@gmail.com>
2025-04-09 16:12:15 +0300

    fr

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
  <section className="w-full h-[80vh] flex-col gap-3 mt-[2rem] justify-center items-center">
   <h1 className="">
-    <link rel="stylesheet" href="" className="" />
+    <Link href={'/post'} className=""> </Link>
   </h1>
  </section>
   )
--- app/post/page.jsx
+++ app/post/page.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 
-const page = () => {
-  return (
-    <section className="w-full h-[80vh] flex gap-3 mt-[2rem] justify-center items-center">
-  <article className="w-full max-w-[1100px]">
-    <h1 className="font-semibold text-lg text-center w-full">Posts</h1>
-    <section className="">
-      <h1 className="">post 1</h1>
-    </section>
-
-  </article>
-</section>
-  )
+const page = async() => {
+    try {
+    
+        return (
+            <section className="w-full h-[80vh] flex gap-3 mt-[2rem] justify-center items-center">
+          <article className="w-full max-w-[1200px]">
+            <h1 className="font-semibold text-lg text-center w-full">Posts</h1>
+            <section className="">
+              <h1 className="">post 1</h1>
+            </section>
+        
+          </article>
+        </section>
+          )  
+    } catch (error) {
+        
+    }
+ 
 }
 
 export default page
