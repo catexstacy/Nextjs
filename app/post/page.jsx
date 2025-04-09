eefc8f82086f12b81b7db2de24b33714d451579b
catexstacy <stacycatex@gmail.com>
2025-04-09 16:49:35 +0300

    commit

--- app/post/page.jsx
+++ app/post/page.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 const page = async() => {
     try {
-    
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json();
         return (
             <section className="w-full h-[80vh] flex gap-3 mt-[2rem] justify-center items-center">
           <article className="w-full max-w-[1200px]">
             <h1 className="font-semibold text-lg text-center w-full">Posts</h1>
-            <section className="">
+            <section className="mt-">
               <h1 className="">post 1</h1>
             </section>
         
